64cf5c4af93244fc3566c516907d1d3ddd851ee7
unix4 <69163299+unix4@users.noreply.github.com>
2021-01-20 13:43:50 +0100

    Create Exercise_1.cpp
    Exercise_1

---
+++ c++/guide/chapter1/Exercise_1.cpp
@@ -0,0 +1,22 @@
+/*
+	Write a C++ program that outputs the following text on screen:
+	
+	Oh what
+	a happy day!
+	Oh yes,
+	what a happy day!
+	
+	Use the manipulator endl where appropriate.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Oh what" << std::endl;
+	std::cout << "a happy day!" << std::endl;
+	std::cout << "Oh yes," << std::endl;
+	std::cout << "what a happy day!" << std::endl;
+	
+	return 0;
+}
