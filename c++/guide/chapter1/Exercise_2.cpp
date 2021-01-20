81191044c7b22b0ced6692e0a28e1149ea114e78
unix4 <69163299+unix4@users.noreply.github.com>
2021-01-20 13:49:03 +0100

    Create Exercise_2.cpp
    Exercise_2

---
+++ c++/guide/chapter1/Exercise_2.cpp
@@ -0,0 +1,31 @@
+/*
+ The following program contains several errors:
+ 
+ Now you should not forget your glasses //
+#include <stream>
+int main
+{
+cout << "If this text",
+cout >> " appears on your display, ";
+cout << " endl;"
+cout << 'you can pat yourself on '
+<< " the back!" << endl.
+return 0;
+)
+
+
+Resolve the errors and run the program to test your changes.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		cout << "If this text";
+		cout << "appears on your display, " << endl;
+		cout << "You can pat yourself on the back! " << endl;
+		
+		return 0;
+}
