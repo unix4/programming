d38500508f006b2e1ddf7802b4ae2dc057261bd5
unix4 <69163299+unix4@users.noreply.github.com>
2021-01-21 14:04:05 +0100

    Create Exercise_2_2.cpp
    Exercise2_2

---
+++ c++/guide/chapter2/Exercise_2_2.cpp
@@ -0,0 +1,26 @@
+/*Exercise 2
+Write a C++ program to generate the screen output shown on the opposite
+page
+
+	I
+		"RUSH"
+				\TO\
+		AND
+	/FRO/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\tI" << endl; 
+	cout << "\t\t\"RUSH\"" << endl;
+	cout << "\t\t\t\\\TO\\" << endl;
+	cout << "\t\t\AND0" << endl;
+	cout << "\t\\FRO\\" << endl;
+	
+	return 0;	
+}
+
