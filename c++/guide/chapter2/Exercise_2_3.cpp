f6f4511324999e8ccd9a58f8d0ef71e70f35f27f
unix4 <69163299+unix4@users.noreply.github.com>
2021-01-21 15:30:18 +0100

    Create Exercise_2_3.cpp
    Exercise2_3

---
+++ c++/guide/chapter2/Exercise_2_3.cpp
@@ -0,0 +1,17 @@
+/*
+ Exercise 3
+Which of the variable definitions shown on the opposite page is invalid or does
+not make sense?
+
+ int a(2.5); does not make sense because of decimal.
+ const long large; invalid
+int b = '?'; does not make sense
+char c('\''); invalid
+char z(500); does not make sense
+unsigned char ch = '\201'; does not make sense
+int big = 40000; valid
+unsigned size(40000); invalid
+double he's(1.2E+5); invalid because of variable name
+ float val = 12345.12345; valid, but decimal gets reduced
+ */
+ 
