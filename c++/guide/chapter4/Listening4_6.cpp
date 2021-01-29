15358beed9f12240e00b7e152442254ba0c07e75
unix4 <69163299+unix4@users.noreply.github.com>
2021-01-29 08:04:38 +0100

    Create Listening4_6.cpp
    Listening4_6

---
+++ c++/guide/chapter4/Listening4_6.cpp
@@ -0,0 +1,37 @@
+// Enter hexadecimal digits and a floating-point number
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std; 
+
+int main()
+{
+	int number = 0;
+	cout << "\nEnter a hexadecimal number: " << endl;
+	cin >> hex >> number; // Input hex-number
+	
+	cout << "Your decimal input: " << number << endl;
+	
+	// If an invalid input occured:
+	cin.sync(); 		// Clears the buffer
+	cin.clear(); 		// Reset error flags
+	
+	double x1 = 0.0, x2 = 0.0;
+	
+	cout << "\nNow enter two floating-point values: " << endl;
+	cout << "1. number: ";
+	cin >> x1;
+	cout << "2. number: ">
+	cin >> x2;
+	
+	cout << fixed << setprecision(2)
+		 << "\nThe sum of both numbers:   "
+		 << setw(10) << x1 + x2 << endl;
+		 
+	cout << "\nThe product of both numbers: "
+	     << setw(10) << x1 * x2 << endl;
+	     
+	return 0;
+}
