8a4e09da8c3e5a4f2ef707e4b207fa812cd421b4
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-02 23:36:28 +0100

    Create Listening4_7.cpp
    Listening4_7

---
+++ c++/guide/chapter4/Listening4_7.cpp
@@ -0,0 +1,32 @@
+// Reads a text with the operator >>
+// and the function getline().
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string header = "   --- Demonstrates Unformatted Input ---";
+
+int main()
+{
+	string word, rest;
+	
+	cout << header
+		 << "\n\nPress <return> to go on" << endl;
+		 
+		 cin.get(); // Read the new line
+		 			// without saving
+			
+		 cout << "\nPlease enter a sentence with several words!"
+			  << "\nEnd with <!> and <return>."
+			  << endl;
+			  
+		 cin >> word; 				// Read the first word
+		 getline(cin, rest, '!');  // and the remaining text
+		 						    // up to the character!
+		 cout << "\nThe first word:   " << word
+		      << "\nRemaining text: " << rest << endl;
+		      
+	return 0; 
+}
