fbab71afb2519f68973a391ff93bdf58dc1b873c
unix4 <69163299+unix4@users.noreply.github.com>
2021-04-08 12:29:40 +0200

    Create output.c

---
+++ c_programming/02/tutorials/output.c
@@ -0,0 +1,13 @@
+// File output.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("There once was a man named George \n");
+  printf("He was 70 years old .\n");
+  printf("He really liked the name George\n");
+  printf("but dit not like being 70. \n");
+  return 0;
+}
