ba676e7a4e10a93e92a13c6608453322d729a098
unix4 <69163299+unix4@users.noreply.github.com>
2021-04-08 12:20:11 +0200

    Create shape.c

---
+++ c_programming/02/tutorials/shape.c
@@ -0,0 +1,15 @@
+// File shape.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("    /| \n");
+  printf("   / | \n");
+  printf("  /  |\n");
+  printf(" /   |\n");
+  printf("/____|\n");
+  
+  return 0;
+}
