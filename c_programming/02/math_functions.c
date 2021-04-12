3f9a05c87f423c0f4c057f9e47391c0e40821e72
unix4 <69163299+unix4@users.noreply.github.com>
2021-04-12 14:39:04 +0200

    Create math_functions.c

---
+++ c_programming/02/math_functions.c
@@ -0,0 +1,14 @@
+// File math_functions.c
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  printf("%f\n", pow(2,3));
+  printf("%f\n", sqrt(36));
+  printf("%f\n", ceil(36.345));
+  printf("%f\n", floor(36.345));
+
+  return 0;
+}
